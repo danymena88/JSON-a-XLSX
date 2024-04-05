0979f6c9a82aa55007921aafd90afad786f8beff
Daniel Mena - Codeffee <28245781+danymena88@users.noreply.github.com>
2024-04-05 11:05:12 -0600

    presentacion a conta

--- jsonToXlsx.py
+++ jsonToXlsx.py
@@ -8,7 +8,7 @@ import xlsxwriter # Módulo para crear archivos XLSX
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__() # Constructor de la clase para la interfaz gráfica
-
+        self.listaDeDocs = []
         self.title("JSON a XLSX")
         self.geometry("450x240")
         self.resizable(False,False)
@@ -37,23 +37,21 @@ class App(customtkinter.CTk):
         self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
 
     def convertirJSON(self): # Método principal para la conversión
-        self.workbook = xlsxwriter.Workbook('DTEs.xlsx')
+        self.listaDeDocs = []
+        self.workbook = xlsxwriter.Workbook('Items.xlsx')
         self.worksheet = self.workbook.add_worksheet()
         list_of_files = self.get_list_of_json_files() # Obteniendo lista de archivos JSON en la carpeta 'archivosJson'
         fila = 1
-        try:
-            self.primeraFila()
-            for file in list_of_files:
-                col = 0
-                row = self.create_list_from_json(f'archivosJson/{file}') # Llamada del método para abrir los JSON
-                for item in row:
-                    self.worksheet.write(fila, col, row[col])
-                    col += 1
-                fila += 1
-            self.worksheet.autofit()
-            self.workbook.close()
-        except:
-            print("Archivo abierto")
+        docnum = 1
+        bold = self.workbook.add_format({'bold' : True})
+        money = self.workbook.add_format({'num_format':'"$" #,##0.00'})
+        self.primeraFila(bold)
+        for file in list_of_files:
+            fila = self.create_list_from_json(f'archivosJson/{file}',fila, docnum, money) # Llamada del método para abrir los JSON
+            docnum += 1
+        self.worksheet.autofit()
+        self.workbook.close()
+        self.archivoDeDtes()
 
     
     def get_list_of_json_files(self):
@@ -65,38 +63,192 @@ class App(customtkinter.CTk):
                     if archivo[-5:-1] != ".jso":
                         list_of_files.remove(archivo)
                 bandera = False
-                print(list_of_files)
                 return list_of_files
             except:
                 os.mkdir('archivosJson')
 
-    def primeraFila(self):
-        primFila = ["Nombre del Emisor","NIT Emisor","NRC","Nombre Comercial","SubTotal"]
+    def primeraFila(self, bold):
+        primFila = ["Num_Documento","Num_Item","Cantidad","Descripción","Precio Unitario","ventaGravada"]
         for file in primFila:
             col = 0
             for item in primFila:
-                self.worksheet.write(0, col, primFila[col])
+                self.worksheet.write(0, col, primFila[col], bold)
                 col += 1
 
-    def create_list_from_json(self,jsonfile):
-        with open(jsonfile, 'r', encoding='utf-8-sig', errors='replace') as f:
-            data = json.load(f)
-        
+    def create_list_from_json(self,jsonfile,fila,docnum,money):
+        print(jsonfile)
         data_list = []
+        try:
+            with open(jsonfile, 'r', encoding='utf-8-sig', errors='replace') as f:
+                data = json.load(f)
+            item = []
+            if len(data) > 0:
+                try:
+                    data_list.append(docnum)
+                    data_list.append(data["identificacion"].get("numeroControl", "NULL"))
+                    data_list.append(data["identificacion"].get("codigoGeneracion", "NULL"))
+                    data_list.append(data["identificacion"].get("selloRecepcion", "NULL"))
+                    data_list.append(data["identificacion"].get("fecEmi", "NULL"))
+                    data_list.append(data["emisor"].get("nombre","NULL"))
+                    data_list.append(data["emisor"].get("nit", "NULL"))
+                    data_list.append(data["emisor"].get("nrc", "NULL"))
+                    #try:
+                    print(data["cuerpoDocumento"][0]["numItem"])
+                    print(len(data["cuerpoDocumento"]))
+                    data_list.append(data["resumen"]["totalGravada"])
+                    try:
+                        for tributo in data["resumen"]["tributos"]:
+                            if tributo["codigo"] == "20":
+                                data_list.append(tributo.get("valor", "NULL"))
+                    except:
+                        data_list.append(data["resumen"].get("totalIva","NULL"))
+                    data_list.append(jsonfile)
+                    self.listaDeDocs.append(data_list)
+                    for uno in data["cuerpoDocumento"]:
+                        item = []
+                        item.append(docnum)
+                        item.append(uno["numItem"])
+                        item.append(uno["cantidad"])
+                        item.append(uno.get("descripcion","NULL"))
+                        item.append(uno.get("precioUni","NULL"))
+                        item.append(uno.get("ventaGravada","NULL"))
+                        col = 0
+                        for dato in item:
+                            if col > 2:
+                                self.worksheet.write(fila, col, dato, money)
+                                col += 1
+                            else:
+                                self.worksheet.write(fila, col, dato)
+                                col += 1
+                        fila += 1
+                    return fila                            
+                    """except:
+                        print("fallo")
+                        for i in range(1,10):
+                            data_list.append("")
+                        data_list.append(f'Error al leer items del archivo: {jsonfile}')
+                        self.listaDeDocs.append(data_list.copy())
+                        data_list = []
+                        data_list.append(docnum)
+                        for x in range(1,6):
+                            data_list.append("")
+                        data_list.append(f'Error al leer items del archivo: {jsonfile}')
+                        col = 0
+                        for dato in data_list:
+                            self.worksheet.write(fila, col, dato)
+                            col += 1
+                        fila += 1
+                        return fila"""
+                except:
+                    try:
+                        data_list = []
+                        data_list.append(docnum)
+                        data_list.append(data["identificacion"].get("numeroControl", "NULL"))
+                        data_list.append(data["identificacion"].get("codigoGeneracion", "NULL"))
+                        data_list.append(data["identificacion"].get("selloRecepcion", "NULL"))
+                        data_list.append(data["identificacion"].get("fecEmi", "NULL"))
+                        data_list.append(data["emisor"].get("nombre","NULL"))
+                        data_list.append(data["emisor"].get("nit", "NULL"))
+                        data_list.append(data["emisor"].get("nrc", "NULL"))
+                        data_list.append(data["cuerpoDocumento"].get("subTotal", "NULL"))
+                        data_list.append(data["cuerpoDocumento"].get("iva", "NULL"))
+                        data_list.append(jsonfile)
+                        self.listaDeDocs.append(data_list)
+                        item = []
+                        item.append(docnum)
+                        item.append(1)
+                        item.append(1)
+                        item.append("Documento contable de liquidación")
+                        item.append(data["cuerpoDocumento"].get("subTotal","NULL"))
+                        item.append(data["cuerpoDocumento"].get("iva", "NULL"))
+                        col = 0
+                        for dato in item:
+                            if col > 2:
+                                self.worksheet.write(fila, col, dato, money)
+                                col += 1
+                            else:
+                                self.worksheet.write(fila, col, dato)
+                                col += 1
+                        fila += 1
+                        return fila
+                    except:
+                        for i in range(1,11):
+                            data_list.append("")
+                        data_list.append(f'Error al leer doc de liquidacion del archivo: {jsonfile}')
+                        self.listaDeDocs.append(data_list.copy())
+                        data_list = []
+                        data_list.append(docnum)
+                        for x in range(1,6):
+                            data_list.append("")
+                        data_list.append(f'Error al leer doc de liquidacion del archivo: {jsonfile}')
+                        col = 0
+                        for dato in data_list:
+                            self.worksheet.write(fila, col, dato)
+                            col += 1
+                        fila += 1
+                        return fila
+            else:
+                data_list = []
+                data_list.append(docnum)
+                for i in range(1,11):
+                    data_list.append("")
+                data_list.append(f'Archivo vacio: {jsonfile}')
+                self.listaDeDocs.append(data_list.copy())
+                data_list = []
+                data_list.append(docnum)
+                for x in range(1,6):
+                    data_list.append("")
+                data_list.append(f'Archivo vacio: {jsonfile}')
+                col = 0
+                for dato in data_list:
+                    self.worksheet.write(fila, col, dato)
+                    col += 1
+                fila += 1
+                return fila
+        except:
+            print("falló al abrir")
+            data_list = []
+            data_list.append(docnum)
+            for i in range(1,10):
+                data_list.append("")
+            data_list.append(f'Error al abrir el archivo: {jsonfile}')
+            self.listaDeDocs.append(data_list.copy())
+            data_list = []
+            data_list.append(docnum)
+            for x in range(1,6):
+                data_list.append("")
+            data_list.append(f'Error al abrir el archivo: {jsonfile}')
+            col = 0
+            for dato in data_list:
+                self.worksheet.write(fila, col, dato)
+                col += 1
+            fila += 1
+            return fila
 
-        if len(data) > 0:
-            data_list.append(data["emisor"]["nombre"])
-            data_list.append(data["emisor"]["nit"])
-            data_list.append(data["emisor"]["nrc"])
-            data_list.append(data["emisor"]["nombreComercial"])
-            try:
-                data_list.append(data["cuerpoDocumento"]["subTotal"])
-            except:
-                data_list.append(data["resumen"]["subTotal"])
-            return data_list
-        else:
-            data_list.append("0000-0")
-            return data_list
+        
+    def archivoDeDtes(self):
+        self.workbook = xlsxwriter.Workbook('DTEs.xlsx')
+        self.worksheet = self.workbook.add_worksheet()
+        bold = self.workbook.add_format({'bold' : True})
+        money = self.workbook.add_format({'num_format':'"$" #,##0.00'})
+        primFila = ["Num_Documento","Número de Control","Código Generación","Sello de Recepción","Fecha de Emisión","Nombre del Emisor","NIT Emisor","NRC","TotalGravada","IVA","Archivo JSON"]
+        col = 0
+        for item in primFila:
+            self.worksheet.write(0, col, item, bold)
+            col += 1
+        fila = 1
+        for row in self.listaDeDocs:
+            col = 0
+            for item in row:
+                if col > 7:
+                    self.worksheet.write(fila, col, item, money)
+                    col += 1
+                else:
+                    self.worksheet.write(fila, col, item)
+                    col += 1
+            fila += 1
+        self.worksheet.autofit()
+        self.workbook.close()
 
 
     
